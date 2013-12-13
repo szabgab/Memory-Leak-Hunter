f67dc12ca31ba4ebd9126a54e8579fd5794285eb
Gabor Szabo <gabor@szabgab.com>
2013-12-13 14:25:39 +0200

    dzil and other files

--- lib/Memory/Leak/Hunter.pm
+++ lib/Memory/Leak/Hunter.pm
@@ -62,6 +62,29 @@ sub report {
 	return $str;
 }
 
+=head1 NAME
+
+Memory::Leak::Hunter - help to find memory leaks
+
+=head1 SYNOPSIS
+
+Experimantal usage of L<Devel::Gladiator>
+
+=head1 AUTHOR
+
+Gabor Szabo L<http://szabgab.com/>
+
+=head COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2013 by Gabor Szabo L<http://szabgab.com/>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
+=cut
+
+
+
 
 1;
 
